e8660d820354ab1e6c03244e87f3ad9ff0c9024f
ivan6799 <ivan6799@gmail.com>
2015-02-14 15:02:30 +0800

    Clear project

--- Project_Cars/Car.py
+++ Project_Cars/Car.py
@@ -1,11 +1,11 @@
-import pygame, sys, random, os
+import pygame, sys, random, os, math
 from pygame.locals import *
 from Classes.Vector import Vector
 from Util.loads import load_image
 
 MOVE = 0
-MOVE_LEFT = 1
-MOVE_RIGHT = 2
+TURN_LEFT = 1
+TURN_RIGHT = 2
 MOVE_UP = 3
 MOVE_DOWN = 4
 
@@ -16,11 +16,12 @@ class Car:
         self.image = load_image('racecar.png', alpha_cannel=True, path='../Images')
         self.image = pygame.transform.rotate(self.image, -90)
         self.rect = self.image.get_rect()
-        self.pos = [pos[0],pos[1]] #Позиция/кординаты объекта на экране
+        self.pos = Vector(pos)
         self.rect.topleft = pos
         self.acsel = Vector((0,0))
         # self.d_acsel = 0
-        self.speed = Vector((50,50))
+        self.speed = Vector((10,0))
+        self.angle_speed = 40
         self.status = MOVE
 
     def event(self, event):
@@ -32,8 +33,15 @@ class Car:
                 self.acsel = self.speed.normalize()* 2
             elif event.key == K_DOWN:
                  self.acsel = self.speed.normalize()* -2
+            elif event.key == K_LEFT:
+                self.status = TURN_LEFT
+            elif event.key == K_RIGHT:
+                self.status = TURN_RIGHT
         elif event.type == KEYUP:
             self.acsel = Vector((0,0))
+            self.status = MOVE
+
+
 
     def aclerate(self):
         self.speed = self.speed + self.acsel
@@ -44,13 +52,10 @@ class Car:
         Передвигаем объект
         """
         self.aclerate()
+        print("rect.x before =",self.rect.x)
         self.rect.y = self.rect.y + (self.speed.y*(dt/1000))
         self.rect.x = self.rect.x + (self.speed.x*(dt/1000))
-
-    def change_acsel_and_speed(self):
-        self.acsel += self.d_acsel
-        self.speed.x+=self.acsel
-
+        print(self.speed, self.rect.topleft)
 
 
     def update(self, dt):
@@ -58,7 +63,12 @@ class Car:
         Обновляем состояние(местоположение, угол поворота и т.п.) объекта
         Этот метод должен вызываться перед отрисовкой каждого кадра
         Как правило, из данного метода вызываются другие методы, которые изменяют нужное состояние объекта
+
         """
+        # if self.status == TURN_RIGHT:
+        #     self.speed.rotate(self.angle_speed/1000*dt)
+        # elif self.status == TURN_LEFT:
+        #     self.speed.rotate(-self.angle_speed/1000*dt)
         self.move(dt)
 
 
@@ -67,9 +77,14 @@ class Car:
         """
         Отрисовываем объект на поверхность screen
         """
+        angle_of_rotate = math.degrees(math.acos(self.speed.normalize().x))
+        if self.speed.y>0:
+            angle_of_rotate = 360-angle_of_rotate
 
-        # pygame.draw.line(screen, (255,0,0), False, [self.rect.center, (self.rect.centerx+self.speed[0], self.rect.centery+self.speed[1])])
-        screen.blit( self.image, self.rect)
+        rotated_img = pygame.transform.rotate(self.image, angle_of_rotate)
+        rect_img = rotated_img.get_rect()
+        rect_img.center = self.rect.center
+        screen.blit(rotated_img, rect_img)
         pygame.draw.lines(screen, (255,0,0), False, [self.rect.center,  (self.rect.centerx+self.speed.x, self.rect.centery+self.speed.y)])
 
 
@@ -85,7 +100,7 @@ test = Car((100,100))
 
 done = False
 while not done:
-    i = 0
+    # i = 0
     for e in pygame.event.get():
 
         if e.type == pygame.QUIT :
